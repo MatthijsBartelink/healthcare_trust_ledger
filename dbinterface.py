a17a82f57d114f84ee1ae967ab2625c56208f93a
Matthijs Bartelink <MatthijsBartelink@gmail.com>
2021-01-12 13:30:41 +0100

    working initial ledger setup with multi-db solution

--- dbinterface.py
+++ dbinterface.py
@@ -1,13 +1,16 @@
 """
 This file contains simple getters and setters for database interaction. Any
-repeated SQL query should be implemented here.
+repeated SQL query should be implemented here. Note that the connection with the
+database is assumed for these functions, and must be passed.
 """
+import sqlite3 as sl
 
 def getContext():
     context = ""
-    data = con.execute("SELECT * FROM ENVIRONMENT WHERE id = 1")
-    for line in data:
-        context = line
+    with sl.connect('trustledgers.db') as con:
+        data = con.execute("SELECT * FROM ENVIRONMENT WHERE id = 1")
+        for line in data:
+            context = line
     return context
 
 # def updatecontext(name, num_ledgers):
@@ -15,15 +18,36 @@ def getContext():
 #         con.execute("UPDATE ENVIRONMENT SET name = ? , num_ledgers = ? WHERE id = ?", (name, num_ledgers, 1))
 
 def setName(name):
-    con.execute("UPDATE ENVIRONMENT SET name = ? WHERE id = ?", (name, 1))
+    with sl.connect('trustledgers.db') as con:
+        con.execute("UPDATE ENVIRONMENT SET name = ? WHERE id = ?", (name, 1))
 
 def setNumledgers(num_ledgers):
-    con.execute("UPDATE ENVIRONMENT SET num_ledgers = ? WHERE id = ?", (num_ledgers, 1))
+    with sl.connect('trustledgers.db') as con:
+        con.execute("UPDATE ENVIRONMENT SET num_ledgers = ? WHERE id = ?", (num_ledgers, 1))
+
+def getEndpointContext(endpoint):
+    context = ""
+    with sl.connect(str(endpoint)+".db") as con:
+        data = con.execute("SELECT * FROM CONTEXT WHERE id = 1")
+        for line in data:
+            context = line
+    return context
 
 def addBlock(block, endpoint):
-    ledgerdata = ""
-    data = con.execute("SELECT * FROM LEDGERS WHERE name = ?", endpoint)
-    for line in data:
-        ledgerdata = line
-    con.execute('INSERT INTO ? (id, name, num_ledgers) values(?, ?, ?)', (endpoint, name, ledgerdata[2] + 1))
-    con.execute("UPDATE LEDGERS SET length = ? WHERE name = ?", (ledgerdata[2] + 1, endpoint))
+
+    context = getEndpointContext(endpoint)
+    # ledgerdata = ""
+    # with sl.connect('trustledgers.db') as con:
+    #     data = con.execute("SELECT * FROM LEDGERS WHERE name = ?", endpoint)
+    #     for line in data:
+    #         ledgerdata = line
+    #     con.execute("UPDATE LEDGERS SET length = ? WHERE name = ?", (ledgerdata[2] + 1, endpoint))
+
+    with sl.connect(str(endpoint)+".db") as con:
+        # increment length of blockchain by one
+        con.execute("UPDATE CONTEXT SET length = ? WHERE id = 1", (context[2]+1, ))
+        # add block to db
+        con.execute("INSERT INTO BLOCK (id, block_json) values (?, ?)", (context[2]+1, block.toJSON()))
+
+# def isInLedgers(con, endpoint):
+#     data = con.execute()
--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import dbinterface
 
 from flask import Flask
 import sqlite3 as sl
+
 from datetime import datetime
+from hashlib import sha256
 
 app = Flask(__name__)
-con = sl.connect('trustledgers.db')
 
 endorsed_key = "This is a replacement for an actual key. Theoretically, any text could be guaranteed this way"
 
@@ -30,34 +31,48 @@ def trust(endpoint):
     """
     Establish this whitebox as a endorser for endpoint.
     """
+
     newLedger = True #TODO: handle ledger which exists elsewhere
 
     if newLedger:
-        setupNewLedger(endpoint)
-        return
+        msg = setupNewLedger(endpoint)
+        # TODO: register new ledger  with nameserver
+        return msg
 
     return 'Not yet implemented'
 
 
 def setupNewLedger(endpoint):
-    with conn:
-        context = dbinterface.getContext()
-        # make ledgers entry
-        con.execute('INSERT INTO LEDGERS (id, name, length) values(?, ?, ?)', (context[2]+1, endpoint, 0))
-        # make table for blocks
+    context = dbinterface.getContext()
+    # make ledgers entry
+    with sl.connect('trustledgers.db') as con:
+        con.execute('INSERT INTO LEDGERS (id, name) values(?, ?)', (context[2]+1, endpoint))
+    # make db for new ledger
+    with sl.connect(str(endpoint)+".db") as con:
         con.execute("""
-            CREATE TABLE ? (
+            CREATE TABLE BLOCK (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 block_json TEXT
             )
-        """, endpoint)
+        """)
+        con.execute("""
+            CREATE TABLE CONTEXT (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                endpoint TEXT,
+                length INTEGER,
+                key TEXT
+            )
+        """)
+        con.execute("INSERT INTO CONTEXT (id, endpoint, length, key) values(?, ?, ?, ?)", (1, endpoint, 0, endorsed_key))
 
-        dbinterface.setNumledgers(context[2]+1)
+    dbinterface.setNumledgers(context[2]+1)
 
-        #make first block
-        now = datetime.now()
-        timestamp = datetime.timestamp(now)
-        keyhash = sha256(endorsed_key.encode()).hexdigest()
+    #make first block
+    now = datetime.now()
+    timestamp = datetime.timestamp(now)
+    keyhash = sha256(endorsed_key.encode()).hexdigest()
 
-        newblock = Block(1, timestamp, 1, keyhash, context[1], endpoint)
-        dbinterface.addBlock(newblock, endpoint)
+    newblock = Block(1, timestamp, 1, keyhash, context[1], endpoint)
+    dbinterface.addBlock(newblock, endpoint)
+
+    return "new trust link and ledger for {}".format(endpoint)
