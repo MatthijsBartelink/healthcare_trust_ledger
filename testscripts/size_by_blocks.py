aa307dc27d4ba8da206a659ab4b43e22d0c98e72
Matthijs Bartelink <MatthijsBartelink@gmail.com>
2021-01-27 17:18:04 +0100

    testscripts

--- testscripts/size_by_blocks.py
+++ testscripts/size_by_blocks.py
@@ -1,10 +1,11 @@
 from block import Block
 from datetime import datetime
 from hashlib import sha256
+from os import stats
 import requests
 
-target = "192.168.2.2:5000"
-blocks_to_push = 1000
+target = "192.168.2.11:5000"
+blocks_to_push = 6000
 block_whitebox = "192.168.2.11:5000"
 endpoint = "endpoint"
 key = "This is a replacement for an actual key. Theoretically, any text could be guaranteed this way"
@@ -14,6 +15,8 @@ id = 1
 block = Block(id, "ADD", datetime.timestamp(datetime.now()), 0, firsthash, block_whitebox, endpoint)
 
 for blocknum in range(blocks_to_push):
+    print("{} {}".format(blocknum, stats('/../WhiteboxServer/endpoint.db')))
+
     request_url = "http://{}/presentblock/{}&{}".format(target, endpoint, block.toJSON())
     r = requests.get(request_url)
 
